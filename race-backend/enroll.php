502a461a336f0f90b1937ccbf435f56ee3fbbfb4
shishir-h <shishirharikripa@gmail.com>
2020-08-03 10:36:54 +0530

    bug fixes

--- race-backend/enroll.php
+++ race-backend/enroll.php
@@ -14,7 +14,7 @@
             
         // }
 
-        $msg = "Hi,\n\n Mr.".$name." is wanting to contact you via Enroll form in your website. Please consider this request and revert to him asap. \n\n The Information Filled by him can be found below\n First Name : ".$fname."\n Last Name : ".$lname."\n Email : ".$mail_id."\n Phone : ".$num."\n Type : ".$type. ".$subject;
+        $msg = "Hi,\n\n Mr.".$name." is wanting to enroll for a service via Enroll form in your website. Please consider this request and revert to him asap. \n\n The Information Filled by him can be found below\n First Name : ".$fname."\n Last Name : ".$lname."\n Email : ".$mail_id."\n Phone : ".$num."\n Type : ".$type.;
         $msg = $msg."\n\nPlease Revert to him ASAP.\nThank You..\n\nRegards,\nDEV Team,\nRaceDS.";
         $msg = wordwrap($msg,70);
 
